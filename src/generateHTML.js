e815e42236d9e816889ca512e3626243d2874435
peoplesm <105310316+peoplesm@users.noreply.github.com>
2022-08-26 13:05:33 -0600

    Fully functionial and some styling

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,13 +1,49 @@
 function createManager(manager) {
-  return `<h1>${manager.name}</h1>`;
+  return `<div class="card" style="width: 18rem">
+  <div class="card-body card-header">
+    <h4 class="card-title">${manager.name}</h4>
+    <h6 class="card-subtitle mb-2"><i class="bi bi-cup-hot"></i> Manager</h6>
+  </div>
+  <div class="card-body">
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">ID: ${manager.id}</li>
+      <li class="list-group-item">Email: <a href="mailto:${manager.email}">${manager.email}</a></li>
+      <li class="list-group-item">Office Number: ${manager.officeNumber}</li>
+    </ul>
+  </div>
+</div>`;
 }
 
 function createEngineer(engineer) {
-  return `<h1>${engineer.name}</h1>`;
+  return `<div class="card" style="width: 18rem">
+  <div class="card-body card-header">
+    <h4 class="card-title">${engineer.name}</h4>
+    <h6 class="card-subtitle mb-2"><i class="bi bi-eyeglasses"></i> Engineer</h6>
+  </div>
+  <div class="card-body">
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">ID: ${engineer.id}</li>
+      <li class="list-group-item">Email: <a href="mailto:${engineer.email}">${engineer.email}</a></li>
+      <li class="list-group-item">GitHub Name: <a href="https://github.com/${engineer.github}">${engineer.github}</a></li>
+    </ul>
+  </div>
+</div>`;
 }
 
 function createIntern(intern) {
-  return `<h1>${intern.name}</h1>`;
+  return `<div class="card" style="width: 18rem">
+  <div class="card-body card-header">
+    <h4 class="card-title">${intern.name}</h4>
+    <h6 class="card-subtitle mb-2"><i class="bi bi-mortarboard"></i> Intern</h6>
+  </div>
+  <div class="card-body">
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">ID: ${intern.id}</li>
+      <li class="list-group-item">Email: <a href="mailto:${intern.email}">${intern.email}</a></li>
+      <li class="list-group-item">School: ${intern.school}</li>
+    </ul>
+  </div>
+</div>`;
 }
 
 function generateHTML(teamArr) {
@@ -38,12 +74,31 @@ function generateDocPage(empCards) {
       <meta charset="UTF-8" />
       <meta http-equiv="X-UA-Compatible" content="IE=edge" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>Document</title>
+      <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx"
+        crossorigin="anonymous"
+      />
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+      <link href="./css/style.css" rel="stylesheet">
+      <title>Team Profile</title>
     </head>
+    <header>
+        <h1>My Team</h1>
+    </header>
     <body>
-      <h1>HEllo my name is ${empCards}</h1>
+      <div class="container">
+        ${empCards}
+        <script
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
+          integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa"
+          crossorigin="anonymous"
+        ></script>
+      </div>
     </body>
-  </html>`;
+  </html>
+  `;
 }
 
 module.exports = generateHTML;
